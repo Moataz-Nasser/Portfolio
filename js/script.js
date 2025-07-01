f9d4e3d2f0c9dfb4d02305f41fac3cab1b5152dd
Moataz <itsmoataznasser@gmail.com>
2025-07-01 05:41:05 +0300

    fixing sending emails

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
-function sendEmail() {
+(function(){
+            emailjs.init({
+                publicKey: "NMTmJ9hmFLh_Z_xSR",
+            });
+})();
+
+const recaptchaToken = grecaptcha.getResponse();
+    if (!recaptchaToken) {
+        alert("Please complete the reCAPTCHA");
+        return;
+}
+
+function sendEmail(event) {
+    event.preventDefault();
+    
     let params = {
-        name: document.getElementById("name"),
-        email: document.getElementById("email"),
-        message: document.getElementById("msg")
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("msg").value,
+        'g-recaptcha-response': recaptchaToken
     }
 
-    emailjs.send("service_gd6xkmt", "template_emzasxc", params);
+    emailjs.send("service_gd6xkmt", "template_emzasxc", params).then(() => {
+            alert("Email sent successfully!");
+            grecaptcha.reset();
+        }, (err) => {
+            alert("Failed to send email: " + JSON.stringify(err));
+        });
 }

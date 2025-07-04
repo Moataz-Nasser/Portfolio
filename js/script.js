70f7b8444dc5ff0191bb65e53345f3b2c3af5bc2
Moataz <itsmoataznasser@gmail.com>
2025-07-04 04:52:43 +0300

    adding light mode and enhancements

--- js/script.js
+++ js/script.js
@@ -1,14 +1,49 @@
-(function(){
-            emailjs.init({
-                publicKey: "NMTmJ9hmFLh_Z_xSR",
-            });
-})();
+// toggle theme colors
 
-const recaptchaToken = grecaptcha.getResponse();
-    if (!recaptchaToken) {
-        alert("Please complete the reCAPTCHA");
-        return;
+const body = document.body;
+const toggleTheme = document.getElementById("toggle-theme");
+const savedTheme = localStorage.getItem('theme');
+const favicon = document.getElementById("favicon");
+
+function setFavicon(src) {
+  favicon.href = `${src}?v=${new Date().getTime()}`;
 }
+
+if(savedTheme && savedTheme === "light"){
+    body.classList.add("light-mode");
+    setFavicon("img/favicon-light.ico");
+}else if(savedTheme && savedTheme === "dark"){
+    body.classList.remove("light-mode");
+    setFavicon("img/favicon.ico")
+}
+
+if(!savedTheme && window.matchMedia('(prefers-color-scheme: light)').matches){
+    body.classList.add("light-mode");
+    setFavicon("img/favicon-light.ico")
+}
+
+toggleTheme.addEventListener("click", () => {
+    body.classList.toggle("light-mode");
+
+    const isLight = body.classList.contains("light-mode");
+
+    localStorage.setItem("theme", isLight? "light" : "dark");
+
+    setFavicon(isLight ? "img/favicon-light.ico" : "img/favicon.ico");
+});
+
+
+// Form validation
+
+document.querySelectorAll('.form-group input, .form-group textarea').forEach(field => {
+    field.addEventListener('blur', () => {
+        field.closest('.form-group').classList.add('touched');
+    });
+});
+
+
+
+emailjs.init("NMTmJ9hmFLh_Z_xSR");
 
 function sendEmail(event) {
     event.preventDefault();
@@ -17,12 +52,10 @@ function sendEmail(event) {
         name: document.getElementById("name").value,
         email: document.getElementById("email").value,
         message: document.getElementById("msg").value,
-        'g-recaptcha-response': recaptchaToken
     }
 
     emailjs.send("service_gd6xkmt", "template_emzasxc", params).then(() => {
             alert("Email sent successfully!");
-            grecaptcha.reset();
         }, (err) => {
             alert("Failed to send email: " + JSON.stringify(err));
         });
